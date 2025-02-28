8654069cc14c115ff3f445d8c5f806ccf34ae298
sagarkarki903 <sagarkarki903@gmail.com>
2025-02-27 20:55:46 -0600

    few changes for hiring

--- fun.js
+++ fun.js
@@ -3,6 +3,7 @@ var noBut = document.querySelector('.no');
 var yesBut = document.querySelector('.yes');
 var backgroundDiv = document.getElementById('backgroundDiv');
 
+
 let move = ()=> {
 
     var randomTop = getRandomValue(window.innerHeight - 100); // Adjusting for container height
@@ -10,11 +11,16 @@ let move = ()=> {
         noBut.style.top = `${randomTop}px`;
         noBut.style.left = `${randomLeft}px`;
 
-
 };
 
 let gifChange = ()=> {
     backgroundDiv.style.backgroundImage = `url("https://media.giphy.com/media/I1GhlA9LXCCouY1lko/giphy.gif")`;
+     // Update button text dynamically
+     document.querySelector(".yes").textContent = "Thank Youuuuuuu!!!!!";
+
+     document.querySelector(".no").style.display = "none";
+
+     document.querySelector(".resumeClass").style.display = "block";
 
 
 };
@@ -28,3 +34,8 @@ function getRandomValue(max) {
 
 noBut.addEventListener('mouseenter', move);
 yesBut.addEventListener('click', gifChange);
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".yes button").textContent = resumeLink;
+});
+
